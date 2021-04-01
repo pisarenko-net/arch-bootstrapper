6a522ca4abccca96c4aff3d24c6e79549d4c0dac
Sergey Pisarenko <sergey@pisarenko.net>
2021-04-01 20:48:07 +0200

    Create git.io handle git.io/prepare_main_install_sergey

--- common/layers/200-prepare_main_install.sh
+++ common/layers/200-prepare_main_install.sh
@@ -2,8 +2,8 @@
 
 # Prepare for main installation.
 
-# run and execute from specific configuration scripts: $ curl -L git.io/apfel_stage1 | sh
-# (created with: $ curl -i https://git.io -F "url=https://raw.githubusercontent.com/pisarenko-net/arch-bootstrap-scripts/master/common/stage1.sh" -F "code=apfel_stage1")
+# run and execute from specific configuration scripts: $ curl -L git.io/prepare_main_install_sergey | sh
+# (created with: $ curl -i https://git.io -F "url=https://raw.githubusercontent.com/pisarenko-net/arch-bootstrapper/main/common/layers/200-prepare_main_install.sh" -F "code=prepare_main_install_sergey")
 
 # install git-secret
 cd /home/${USER}
