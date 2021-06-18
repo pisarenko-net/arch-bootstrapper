3f05fe697628de713873e566f850a64ff61fd161
Sergey Pisarenko <sergey@pisarenko>
2021-06-18 18:56:13 +0000

    update cddev install script

--- physical/ch.cddev/100-install.sh
+++ physical/ch.cddev/100-install.sh
@@ -29,50 +29,17 @@ $AS /usr/bin/cp -R /tmp/scripts-repo/common/private /tmp/private
 $AS /usr/bin/cp -R /tmp/scripts-repo/physical/ch.cddev/private/* /tmp/private/
 $AS /usr/bin/rm /tmp/private/*secret
 
-$AS /usr/bin/git clone https://github.com/pisarenko-net/wallpapers.git /tmp/wallpapers
-
 eval "`/usr/bin/curl -L git.io/install_cli_sergey`"
 eval "`/usr/bin/curl -L git.io/install_xorg_sergey`"
-
-export INSTALL_VMS="example_cli"
-
-echo '==> Installing custom apps'
-/usr/bin/cp /tmp/apps/vm_refresh_packer /usr/local/bin/
-/usr/bin/chmod +x /usr/local/bin/vm_refresh_packer
-/usr/bin/cp /tmp/apps/vm_rebuild_install /usr/local/bin/
-/usr/bin/chmod +x /usr/local/bin/vm_rebuild_install
 
 echo '==> Installing X driver and enhancements'
 /usr/bin/pacman -S --noconfirm xf86-video-intel compton
 $AS /usr/bin/xfconf-query -c xfwm4 -p /general/use_compositing -s false
 $AS /usr/bin/cp -R /tmp/configs/compton.desktop /home/${LUSER}/.config/autostart/
 
-echo '==> Installing and configuring bluetooth'
-/usr/bin/pacman -S --noconfirm bluez bluez-utils
-/usr/bin/sed -i 's/#AutoEnable=false/AutoEnable=true/' /etc/bluetooth/main.conf
-cd /
-/usr/bin/tar xvzf /tmp/private/bluetooth-pairings.tar.gz
-/usr/bin/systemctl enable bluetooth
-
 echo '==> Enabling better power management'
 /usr/bin/pacman -S --noconfirm tlp
 /usr/bin/systemctl enable tlp
-
-echo '==> Installing VirtualBox, vagrant, packer and scripts'
-/usr/bin/pacman -S --noconfirm virtualbox vagrant packer
-cd /home/${LUSER}
-$AS /usr/bin/git clone git@github.com:pisarenko-net/arch-bootstrapper.git
-
-echo '==> Installing VirtualBox extensions'
-cd /home/${LUSER}
-$AS /usr/bin/git clone https://aur.archlinux.org/virtualbox-ext-oracle.git
-cd virtualbox-ext-oracle
-$AS /usr/bin/makepkg -si --noconfirm
-cd ..
-$AS /usr/bin/rm -rf virtualbox-ext-oracle
-modprobe vboxdrv
-modprobe vboxnetadp
-modprobe vboxnetflt
 
 echo '==> Install CIFS tools'
 /usr/bin/pacman -S --noconfirm cifs-utils
@@ -89,30 +56,12 @@ echo '==> Installing Arduino tools'
 /usr/bin/mkdir /vm_shared
 /usr/bin/chown ${LUSER} /vm_shared
 
-echo '==> Updating VM templates'
-$AS /usr/local/bin/vm_refresh_packer
-
-echo '==> Building and enabling VMs'
-for vm in ${INSTALL_VMS}
-do
-        echo "==> Building VM ${vm}"
-        /usr/local/bin/vm_rebuild_install ${vm}
-done
-
-echo '==> Committing changes to vagrant/packer repo'
-cd /home/${LUSER}/arch-bootstrapper
-$AS /usr/bin/git add .
-PACKER_VERSION=`date +%Y-%m-01`
-$AS /usr/bin/git commit -m "update Arch packer version to: ${PACKER_VERSION}"
-$AS /usr/bin/git push
-
 echo '==> Cleaning up'
 $AS /usr/bin/gpg --batch --delete-secret-keys 6E77A188BB74BDE4A259A52DB320A1C85AFACA96
 /usr/bin/rm -rf /tmp/scripts-repo
 /usr/bin/rm -rf /tmp/apps
 /usr/bin/rm -rf /tmp/configs
 /usr/bin/rm -rf /tmp/private
-/usr/bin/rm -rf /tmp/wallpapers
 
 echo '==> Updating Last install date in the repo'
 cd /home/${LUSER}/arch-bootstrapper
