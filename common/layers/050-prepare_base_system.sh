0538f62827b308b608f8c1dd43706062be8f0f05
Sergey Pisarenko <sergey@pisarenko.net>
2022-08-07 17:52:37 +0200

    Update 050-prepare_base_system.sh

--- common/layers/050-prepare_base_system.sh
+++ common/layers/050-prepare_base_system.sh
@@ -5,11 +5,11 @@
 # run and execute from configuration scripts (not to be invoked directly): $ curl -L git.io/prepare_base_system_sergey | sh
 # (created with: $ curl -i https://git.io -F "url=https://raw.githubusercontent.com/pisarenko-net/arch-bootstrapper/main/common/layers/050-prepare_base_system.sh" -F "code=prepare_base_system_sergey")
 
-echo "==> Setting local mirror"
-/usr/bin/curl -s -L "$MIRRORLIST" |  sed 's/^#Server/Server/' > /etc/pacman.d/mirrorlist
+#echo "==> Setting local mirror"
+#/usr/bin/curl -s -L "$MIRRORLIST" |  sed 's/^#Server/Server/' > /etc/pacman.d/mirrorlist
 
 echo '==> Bootstrapping the base installation'
-/usr/bin/pacstrap -G ${TARGET_DIR} base base-devel lvm2 linux linux-firmware btrfs-progs netctl neovim dhcpcd openssh grub-efi-x86_64 efibootmgr net-tools intel-ucode wget git tmux mosh
+/usr/bin/pacstrap ${TARGET_DIR} base base-devel lvm2 linux linux-firmware btrfs-progs netctl neovim dhcpcd openssh grub-efi-x86_64 efibootmgr net-tools intel-ucode wget git tmux mosh
 
 echo '==> Generating the filesystem table'
 /usr/bin/cat /tmp/fstab >> "${TARGET_DIR}/etc/fstab"
