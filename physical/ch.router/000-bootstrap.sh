852abe4a121683c54c21e688564e26468d2859b3
Sergey Pisarenko <sergey@pisarenko.net>
2023-11-04 19:36:46 +0100

    Update 000-bootstrap.sh

--- physical/ch.router/000-bootstrap.sh
+++ physical/ch.router/000-bootstrap.sh
@@ -20,6 +20,7 @@ export TARGET_DIR='/mnt'
 export ENC_KEY_PATH="${TARGET_DIR}/enc.key"
 export COUNTRY='CH'
 export MIRRORLIST="https://www.archlinux.org/mirrorlist/?country=${COUNTRY}&protocol=http&protocol=https&ip_version=4&use_mirror_status=on"
+export INSTALL_IFACE="eth2"
 
 eval "`/usr/bin/curl -L git.io/partition_drive_sergey`"
 eval "`/usr/bin/curl -L git.io/prepare_base_system_sergey`"
@@ -27,29 +28,13 @@ eval "`/usr/bin/curl -L git.io/prepare_encryption_sergey`"
 eval "`/usr/bin/curl -L git.io/install_base_system_sergey`"
 eval "`/usr/bin/curl -L git.io/finalize_base_system_sergey`"
 
-export WAN_IFACE="eth0"
-export LAN_IFACE="eth1"
-
-echo '==> Configuring networks'
-/usr/bin/cat <<-EOF > "${TARGET_DIR}/etc/netctl/wan"
-Interface=${WAN_IFACE}
+echo '==> Configuring network using service NIC'
+/usr/bin/cat <<-EOF > "${TARGET_DIR}/etc/netctl/ethernet-dhcp"
+Interface=${INSTALL_IFACE}
 Connection=ethernet
 IP=dhcp
-IP6=stateless
-DNS=('8.8.8.8' '8.8.4.4')
 EOF
-/usr/bin/cat <<-EOF > "${TARGET_DIR}/etc/netctl/trusted_lan"
-Interface=${LAN_IFACE}
-Connection=ethernet
-IP=static
-Address=('192.168.10.1/24')
-
-ForceConnect=yes
-SkipNoCarrier=yes
-EOF
-/usr/bin/arch-chroot ${TARGET_DIR} /usr/bin/netctl enable trusted_lan
-/usr/bin/arch-chroot ${TARGET_DIR} /usr/bin/pacman -S --noconfirm ifplugd socat
-/usr/bin/arch-chroot ${TARGET_DIR} /usr/bin/systemctl enable netctl-ifplugd@eth0.service
+/usr/bin/arch-chroot ${TARGET_DIR} /usr/bin/netctl enable ethernet-dhcp
 
 echo '==> Prepopulating shell history'
 echo 'curl -L git.io/install_ch_router_sergey | sh' >> "${TARGET_DIR}/root/.bash_history"
